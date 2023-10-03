9e27a25773b4b948e9f85e289d5b5f9d822f4cf2
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-10-03 12:29:38 +0530

    initial

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1uKL0ZgpvFcqwFn7dkjxePNPxyHW8UFM",
+  authDomain: "cyber-573c4.firebaseapp.com",
+  databaseURL: "https://cyber-573c4-default-rtdb.firebaseio.com",
+  projectId: "cyber-573c4",
+  storageBucket: "cyber-573c4.appspot.com",
+  messagingSenderId: "30325666876",
+  appId: "1:30325666876:web:add627c05b91532ee43f39"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
